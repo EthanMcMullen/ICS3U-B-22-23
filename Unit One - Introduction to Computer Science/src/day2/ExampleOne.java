5a87da56e843434d27c77047e00a31037027e80a
Ethan McMullen <emcmullen@bayviewglen.ca>
2022-09-09 11:07:38 -0400

    Initial Commit

---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,26 @@
+package day2;
+/*
+ This is a multi line comments 
+ Comments are ignored by the compiler (hava) and are meant for developers to
+ understand what the code is meant to do 
+ */
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Computer Science"); 
+        System.out.println("Welcome to Computer Science"); 
+        System.out.println("Welcome to Computer Science"); 
+
+        // System.out.println(); // This line is ignored
+
+        System.out.print("a");
+        System.out.print("b");
+        System.out.print("c");
+        System.out.println("d");
+        System.out.print("e");
+        System.out.println("f");
+        
+    }    
+}
+
+
+
---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,18 @@
+package day2;
+
+/*
+ * This example is about escape sequences
+ */
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("Computer Scie\nce is the \"BEST\" class ever!");
+        System.out.println("/ \\");
+
+        // Escape sequesnce is meant to give another meaning to a character
+        // Ap exam cares about \" \n & \\
+        // \n is new line
+        // \" Displays a quote
+        // \\ Displays a backslash
+    }
+}
+ 
